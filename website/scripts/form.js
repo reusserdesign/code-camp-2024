18acda68da7efd1300cf7027931eabb1114830c7
Sean Barker <sean.barker@reusser.com>
2024-08-01 22:52:58 -0400

    Add form button

---
+++ website/scripts/form.js
@@ -0,0 +1,4 @@
+var nameInput = document.getElementById("form-name");
+var emailInput = document.getElementById("form-email");
+var commentInput = document.getElementById("form-comment");
+var submitButton = document.getElementById("form-submit");
